6b335f1cefc489e337f38e13f8dff9a3bd059bd8
SumitSharma2210 <sumit.sharma.gkv@gmail.com>
2025-01-14 23:40:28 +0530

    new commit

--- server.js
+++ server.js
@@ -8,9 +8,17 @@ const app = express();
 const server = http.createServer(app);
 const cors = require("cors");
 
+const allowedOrigins = ["http://localhost:3000", process.env.FRONTEND_URL];
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
