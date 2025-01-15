6506c190b0270c8cba22101c6cd0595ba15cad07
SumitSharma2210 <sumit.sharma.gkv@gmail.com>
2025-01-15 11:29:41 +0530

    11.29

--- server.js
+++ server.js
@@ -8,9 +8,23 @@ const app = express();
 const server = http.createServer(app);
 const cors = require("cors");
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:3000",
+  "https://event-management-frontend-delta.vercel.app",
+  "https://event-management-fro-git-37c370-sumit-sharmas-projects-a83a6a98.vercel.app",
+  "https://event-management-frontend-7xel5q27o.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "*",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
